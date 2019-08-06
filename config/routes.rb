5e1185d2d211b08f4f945c9b699d1c41df4610c3
Axel Prevoteaux <axel.prevoteaux@gmail.com>
2019-08-06 16:19:04 +0200

    finish with articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CONVENTION 2
+  # HTTP_VERB 'URL', to: 'CONTROLLER#ACTION'
+  root to: 'pages#home' # always the first one
+
+  # resources :restaurants
+
+  # READ
+  get 'articles',     to: 'articles#index'
+
+  # CREATE
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  post 'articles',    to: 'articles#create'
+
+  # READ BIS
+  get 'articles/:id', to: 'articles#show', as: 'article'
+
+  # UPDATE
+  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  patch 'articles/:id',    to: 'articles#update'
+
+  # DELETE
+  delete 'articles/:id', to: 'articles#destroy'
+
+  # get 'about',   to: 'pages#about'
+  # get 'contact', to: 'pages#contact'
 end
